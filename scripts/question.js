af80047f49a2d0537f068a8df75d43e3484e2eee
Thomas Marstrander <marstranderthomas@gmail.com>
2021-11-26 15:29:04 +0100

    JI-2995 Fix only show submit text if content is standalone

--- scripts/question.js
+++ scripts/question.js
@@ -1312,7 +1312,7 @@ H5P.Question = (function ($, EventDispatcher, JoubelUI) {
         }
       };
 
-      const isSubmitting = extras.contentData
+      const isSubmitting = extras.standalone && extras.contentData
         && (extras.contentData.isScoringEnabled || extras.contentData.isReportingEnabled);
 
       if (isSubmitting && extras.textIfSubmitting) {
